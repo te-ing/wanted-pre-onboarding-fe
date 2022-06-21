139b45fae6a5db4dc07c1cb44d80a77393c32b12
Taejung Kim <hi2177@naver.com>
2022-06-21 15:02:40 +0900

    feat: 더미데이터 API 호출 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,25 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import GNB from '../components/GNB';
 
 const MainPage = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get('/DUMMY.json');
+        setPosts(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPosts();
+  }, []);
+  console.log(posts);
+
   return (
     <Container>
       <GNB />
